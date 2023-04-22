c648b9b97d14a887b73b3e73f0a5980d7d9ff543
strange <rivasjoaquin02@gmail.com>
2023-04-22 16:34:02 -0400

    implementada funcionalidad para la ActiveBar

--- src/components/Monopolies/Companies/MiddleSection/ActiveBar.tsx
+++ src/components/Monopolies/Companies/MiddleSection/ActiveBar.tsx
@@ -1,9 +1,22 @@
 const ActiveBar = ({ id }: { id: number }) => {
+    const getColor = (idx: number): string => {
+        if (idx === id) return "bg-white";
+        return "bg-secondary-100";
+    };
+
     return (
-        <div className=" mx-auto flex h-20 w-full items-center justify-center align-bottom">
-            <div className="mx-2 h-1 w-16 rounded-lg bg-secondary-100 shadow-lg hover:bg-secondary-200"></div>
-            <div className="mx-2 h-1 w-16 rounded-lg bg-white shadow-lg hover:bg-secondary-200"></div>
-            <div className="mx-2 h-1 w-16 rounded-lg bg-secondary-100 shadow-lg hover:bg-secondary-200"></div>
+        <div className=" flex h-20 items-center justify-center ">
+            {Array(3)
+                .fill(0)
+                .map((v, idx) => {
+                    return (
+                        <div
+                            className={`mx-2 h-1 w-16 rounded-lg ${getColor(
+                                idx + 1
+                            )} shadow-lg hover:bg-secondary-200`}
+                        ></div>
+                    );
+                })}
         </div>
     );
 };
